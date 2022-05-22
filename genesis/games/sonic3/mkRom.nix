585a111059dc6a8b63713d9eeebd99d9ce44db5a
bree <me@bree.dev>
2022-05-22 11:20:06 -0400

    `nixpkgs-fmt`

--- genesis/games/sonic3/mkRom.nix
+++ genesis/games/sonic3/mkRom.nix
@@ -6,7 +6,7 @@
 , s3p2bin ? callPackage ../../tools/s3p2bin { }
 }:
 
-{ assembleFlags ? [], ... }@attrs:
+{ assembleFlags ? [ ], ... }@attrs:
 
 stdenv.mkDerivation ({
   src = fetchFromGitHub {
@@ -15,7 +15,7 @@ stdenv.mkDerivation ({
     rev = "dc3b0db93230eb31b8c074a878e9bae37c422724";
     sha256 = "sha256-sMUntm4jcGxHNkzC6o3f7jukg7v221teIE5COA7g/Yo=";
   };
-  
+
   nativeBuildInputs = [ asl python s3p2bin ];
 
   assembleFlags = [ "-x" "-xx" "-n" "-c" "-A" "-L" ] ++ assembleFlags;
